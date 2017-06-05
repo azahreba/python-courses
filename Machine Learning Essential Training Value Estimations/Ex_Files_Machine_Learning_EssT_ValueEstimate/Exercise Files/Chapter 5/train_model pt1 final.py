9b242285553c353f6b79ecfca73864de6e678a5d
Artur Zahreba <artu_zahreba@epam.com>
2017-06-05 11:20:59 +0200

    Ex Files

---
+++ Machine Learning Essential Training Value Estimations/Ex_Files_Machine_Learning_EssT_ValueEstimate/Exercise Files/Chapter 5/train_model pt1 final.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# Load the data set
+df = pd.read_csv("ml_house_data_set.csv")
+
+# Remove the fields from the data set that we don't want to include in our model
+del df['house_number']
+del df['unit_number']
+del df['street_name']
+del df['zip_code']
+
+# Replace categorical data with one-hot encoded data
+features_df = pd.get_dummies(df, columns=['garage_type', 'city'])
+
+# Remove the sale price from the feature data
+del features_df['sale_price']
+
+# Create the X and y arrays
+X = features_df.as_matrix()
+y = df['sale_price'].as_matrix()
